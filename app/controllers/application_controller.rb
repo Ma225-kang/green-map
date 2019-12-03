dd5b70adf5b56aee927e7ec1f11f295339b0ed3c
Maylis Castell <mayliscastell@laposte.net>
2019-12-03 17:48:49 +0100

    Devise parametering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home, :places, :place]
+
+  def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :city, :zip_code, :country, :avatar])
+    end
 end
