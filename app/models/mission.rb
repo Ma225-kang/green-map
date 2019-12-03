0abee35b7230ab4ac89e42ba85f43ee9f8026aeb
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2019-12-03 15:12:54 +0100

    Changed Mission Model and added Upload file

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,14 @@
 class Mission < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  TIME = %w[morning afternoon all-day]
+
+  validates :date, presence: true
+  validates :time_slot, presence: true, inclusion: { in: TIME }
+  validates :mapmaster_photo, presence: true
+  validates :participation_level, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :status, inclusion: { in: %w(planned on-going cancelled completed) }
+
+  mount_uploader :mapmaster_photo, PhotoUploader
 end
