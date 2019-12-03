2cc8a06d64cbfd35066cdde6a965b20dbf087a70
Ophelie Jones <o.cauchye@gmail.com>
2019-12-03 15:19:33 +0100

    validates done on places model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :missions
+
+  STATUS = %w[new on-going clean]
+  TRASHS = %w[plastic dangerous glass metal liquid organic electronic miscellaneous]
+
+  validates :type_of_trash, presence: true, inclusion: { in: TRASHS }
+  validates :volume, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :status, presence: true, inclusion: { in: STATUS }
 end
