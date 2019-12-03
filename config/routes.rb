037d6df365ea7fad964394a15fe153c01398b25d
Maylis Castell <mayliscastell@laposte.net>
2019-12-03 16:31:48 +0100

    Routes validées

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/profile", to: "users#profile", as: :profile
+
+  resources :places, only: [:index, :show, :new, :create] do
+    resources :missions, only: [:create]
+  end
+
+  resources :missions, only: [] do
+    member do
+      get 'review'
+      patch 'complete'
+      patch 'cancel'
+    end
+  end
 end
