57f0390e33b9e64eda648c586152ca309d23718e
Ophelie Jones <o.cauchye@gmail.com>
2019-12-05 14:11:14 +0100

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_103126) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.string "address"
     t.float "latitude"
     t.float "longitude"
     t.string "level"
