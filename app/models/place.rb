53edf2a1cd63037f11ac9497f4bd3e35f8846e5a
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2019-12-06 15:52:49 +0100

    Add form to create new place

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,7 +8,8 @@ class Place < ApplicationRecord
   mount_uploader :mapmaster_photo, PhotoUploader
 
   STATUS = %w[new on-going clean]
-  TRASHES = %w[plastic dangerous glass metal liquid organic electronic miscellaneous]
+  # TRASHES = %w[plastic dangerous glass metal liquid organic electronic miscellaneous]
+  TRASHES = %w[bottle can fish plastic-bottle trash fridge dangerous]
 
   CLEANUP_EQUIPMENT_NEEDED = %w[gloves bag rake wheelbarrow]
   VOLUMES = 0..5
@@ -17,10 +18,8 @@ class Place < ApplicationRecord
 
   validates :trashes_on_site, presence: true
   validates :volume, presence: true, inclusion: { in: VOLUMES }, numericality: { only_integer: true }
+
   # validates :status, presence: true, inclusion: { in: STATUS }
-
-
-
 
   # def equipments
   #   equipments = []
