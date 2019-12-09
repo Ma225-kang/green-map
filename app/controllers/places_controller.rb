6356344a4b0bb05ce27b64caebe80aeacadede34
Maylis Castell <mayliscastell@laposte.net>
2019-12-09 11:11:15 +0100

    cookie fixed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,8 @@ class PlacesController < ApplicationController
   end
 
   def create
+
+    binding.pry
     # COOKIE STUFF
     # if cookie is a hash
     @place.longitude = cookies[:longitude]
@@ -42,6 +44,8 @@ class PlacesController < ApplicationController
   end
 
   def geolocate_user
+    p cookies[:latitude]
+    p cookies[:longitude]
     @markers = [{lat: cookies[:latitude], lng: cookies[:longitude]}]
   end
 
