9675639ba0033c28168092d5404e8d6a47dbe262
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2019-12-09 16:34:29 +0100

    Added icons based on Mapmaster inputs

--- app/models/place.rb
+++ app/models/place.rb
@@ -12,26 +12,37 @@ class Place < ApplicationRecord
   TRASHES = %w[plastic dangerous glass metal liquid organic electronic miscellaneous]
   # TRASHES = %w[bottle can fish plastic-bottle trash fridge dangerous]
 
-  CLEANUP_EQUIPMENT_NEEDED = %w[gloves bag rake wheelbarrow]
+  CLEANUP_EQUIPMENT_NEEDED = ['gloves', 'trash bag', 'rake', 'wheelbarrow', 'litter picker', 'face mask']
   VOLUMES = 0..5
 
   TRANSPORT = %w[backpack car truck]
 
-  validates :trashes_on_site, presence: true
+  validates :trashes_on_site, presence: true, inclusion: { in: TRASHES }
   validates :volume, presence: true, inclusion: { in: VOLUMES }, numericality: { only_integer: true }
 
   # validates :status, presence: true, inclusion: { in: STATUS }
 
-  # def equipments
-  #   equipments = []
+  def equipments
+    equipments = []
 
-  #   if trashes_on_site.include?('organic')
-  #     equipments << 'bag'
-  #   elsif trashes_on_site.include?('dangerous')
-  #     equipments << 'gloves'
-  #   # ...
-  #   end
+    if trashes_on_site.include?('plastic')
+      equipments = ['trash bag', 'litter picker']
+    elsif trashes_on_site.include?('dangerous')
+      equipments = ['trash bag', 'gloves', 'face mask']
+    elsif trashes_on_site.include?('glass')
+      equipments = ['trash bag', 'gloves']
+    elsif trashes_on_site.include?('metal')
+      equipments = ['trash bag', 'gloves']
+    elsif trashes_on_site.include?('liquid')
+      equipments = ['trash bag', 'gloves']
+    elsif trashes_on_site.include?('organic')
+      equipments = ['trash bag', 'litter picker']
+    elsif trashes_on_site.include?('electronic')
+      equipments = ['wheelbarrow']
+    elsif trashes_on_site.include?('miscellaneous')
+      equipments = ['trash bag', 'gloves', 'litter picker']
+    end
 
-  #   return equipments
-  # end
+    return equipments
+  end
 end
