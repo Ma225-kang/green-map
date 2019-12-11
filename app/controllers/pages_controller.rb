0500c50c958313687bc895de3ad79cafbdf64ba5
Maylis Castell <mayliscastell@laposte.net>
2019-12-11 10:45:12 +0100

    Add green marker for clean places
    When a place volume is at 0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,21 @@ class PagesController < ApplicationController
   def home
     @places = Place.all
     @markers = @places.map do |place|
-      {
-        lat: place.latitude,
-        lng: place.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
-        image_url: helpers.asset_url('icons/red_mapmarker_icon.png')
-      }
+      if place.volume != 0
+        {
+          lat: place.latitude,
+          lng: place.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
+          image_url: helpers.asset_url('icons/red_mapmarker_icon.png')
+        }
+      else
+        {
+          lat: place.latitude,
+          lng: place.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
+          image_url: helpers.asset_url('icons/green_mapmarker_icon.png')
+        }
+      end
     end
   end
 end
